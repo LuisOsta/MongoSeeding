94f0ff5153e86ee1f98680ab94aa67d7450963fc
Luis Osta <luisosta1999@gmail.com>
2020-06-08 15:45:52 -0500

    implemented basic api and mongoose integration

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-
-const app = express();
+const mongoose = require("mongoose");
 const PORT = 8080 || process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI || "";
+const app = express();
 app.get("/users", (req, res) => {
   console.log("placeholder");
 });
 
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
+
 app.listen(PORT, () => {
+  console.log(`MONGO URI ${MONGO_URI}`);
   console.log(`API listening on port ${PORT}`);
 });
