d40c77b944c6921ff21716ad7bd070c7fd33ae95
Luis Osta <luisosta1999@gmail.com>
2020-06-09 15:19:15 -0500

    fixed export/import bug

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,13 +1,13 @@
-const mongoose, {Schema} = require("mongoose");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
+const UserSchema = new Schema({
+  name: String,
+  title: String,
+  department: String,
+  joined: Date,
+});
 
-export const UserSchema = new Schema({
-    name: String,
-    title: String,
-    department: String,
-    joined: Date
-})
+const User = mongoose.model("user", UserSchema);
 
-const User = mongoose.model("user", UserSchema)
-
-export default User
+module.exports = User;
